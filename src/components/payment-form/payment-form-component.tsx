721cdae04f6ddbd638f1f09146874b38497524bd
Ben <bennnnnnnvu@gmail.com>
2024-02-11 21:35:36 -0800

    modify gitignore

--- src/components/payment-form/payment-form-component.tsx
+++ src/components/payment-form/payment-form-component.tsx
@@ -1,14 +1,53 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
-import {PaymentButton, PaymentFormContainer} from './payment-form.styles'
+import {PaymentButton, PaymentFormContainer, FormContainer} from './payment-form.styles'
 import { BUTTON_STYLE_CLASSES } from "../Button/button-style-classes";
 
 const PaymentForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handlePayment = async (event) => {
+        event.preventDefault();
+
+        if(!stripe || !elements) return;
+
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({amount: 10000})
+        }).then(response => response.json());
+
+        const {paymentIntent: {client_secret}} = response;
+        console.log(client_secret);
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: "Ben Vu",
+                },
+            },
+        });
+
+        if(paymentResult.error){
+            alert(paymentResult.error)
+        } else {
+            if(paymentResult.paymentIntent.status === 'succeeded') {
+                alert("Payment Successful!")
+            }
+        }
+    };
+
     return (
         <PaymentFormContainer>
-            <h2>Credit Card Payment:</h2>
-            <CardElement></CardElement>
-            <PaymentButton buttonType={BUTTON_STYLE_CLASSES.google}>PAY NOW</PaymentButton>
+            <FormContainer onSubmit={handlePayment}>
+                <h2>Credit Card Payment:</h2>
+                <CardElement></CardElement>
+                <PaymentButton buttonType={BUTTON_STYLE_CLASSES.google}>PAY NOW</PaymentButton>
+            </FormContainer>
         </PaymentFormContainer>
     )
 };
