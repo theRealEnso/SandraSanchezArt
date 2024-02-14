2340a07733bc3b56888320020be41c1f55d83a3c
Ben <bennnnnnnvu@gmail.com>
2024-02-13 17:53:39 -0800

    finished styling the inputs in the checkout form. Also implemented card elements component from stripe/react library and serverless function on netlify to handle payment requests

--- src/routes/Checkout/checkout-component.tsx
+++ src/routes/Checkout/checkout-component.tsx
@@ -1,38 +1,52 @@
-import { useState, useEffect, useRef, MouseEvent, ChangeEvent } from "react";
+import { useState, useEffect, useContext, useRef, MouseEvent, ChangeEvent } from "react";
 
-import {useStripe, useElements} from '@stripe/react-stripe-js'
+import { UserContext } from "../../contexts/user-context";
 
-import { CheckoutContainer, FormContainer, EmailContainer, DeliveryContainer, DropdownContainer, DropdownButton, DropdownContent, DropdownOption, SearchInputContainer, SearchInput, OptionsContainer, NameContainer, AddressContainer, OptionalAddressInputContainer, OptionalAddressText, OptionalAddressInput } from "./checkout-styles";
+import { CheckoutContainer, FormContainer, EmailContainer, DeliveryContainer, CountriesDropdownContainer, CountriesDropdownButton, CountriesDropdownContent, CountriesDropdownOption, SearchInputContainer, SearchInput, CountriesOptionsContainer, NameContainer, AddressContainer, CityStateZipcodeContainer, OptionalAddressInputContainer, OptionalAddressText, OptionalAddressInput, StatesDropdownContainer, StatesDropdownButton, StatesDropdownContent, StatesDropdownOption,  } from "./checkout-styles";
 
 import countries from "../../countries";
+import states from "../../USA-states";
 
 import CheckoutFormInput from "../../components/Checkout-form-input/checkout-form-input-component";
 
 import PaymentForm from "../../components/payment-form/payment-form-component";
 
+import {useStripe, useElements, CardElement} from "@stripe/react-stripe-js";
+
 const Checkout = () => {
 
-    const countriesDropdownRef = useRef<HTMLDivElement | null>(null);
+    const {currentUser} = useContext(UserContext);
 
-    const [isOpen, setIsOpen] = useState(false);
+    const countriesDropdownRef = useRef<HTMLDivElement | null>(null);
+    const stateDropdownRef = useRef<HTMLDivElement | null>(null);
+
+    const [isCountriesSelectDropdownOpen, setIsCountriesSelectDropdownOpen] = useState(false);
+    const [isStateDropdownOpen, setIsStateDropdownOpen] = useState(false);
+    const [countriesArray, setCountriesArray] = useState<string[]>(countries);
     const [selectedCountry, setSelectedCountry] = useState("United States");
     const [searchQuery, setSearchQuery] = useState("");
-    const [countriesArray, setCountriesArray] = useState<string[]>(countries);
+    const [statesArray, setStatesArray] = useState(states);
+    const [selectedState, setSelectedState] = useState('California');
     const [filteredCountries, setFilteredCountries] = useState<string[]>(countriesArray);
     const [formInputs, setFormInputs] = useState({
         email: "",
         firstName: "",
         lastName: "",
         company: "",
-        address: "",
+        streetAddress: "",
+        city: "",
+        state: "",
+        zipOrPostalCode: "",
         additionalAddress: "",
     });
 
-    const {email, firstName, lastName, company, address, additionalAddress} = formInputs;
+    const {email, firstName, lastName, company, streetAddress, city, state, zipOrPostalCode, additionalAddress} = formInputs;
 
     const [optionalAddressInput, setOptionalAddressInput] = useState(false);
 
-    const toggleIsOpen = () => setIsOpen(!isOpen);
+    const toggleCountrySelectDropdown = () => setIsCountriesSelectDropdownOpen(!isCountriesSelectDropdownOpen);
+
+    const toggleStateSelectDropdown = () => setIsStateDropdownOpen(!isStateDropdownOpen);
 
     const toggleOptionalAddressInput = () => setOptionalAddressInput(!optionalAddressInput);
 
@@ -44,11 +58,17 @@ const Checkout = () => {
         });
     };
 
-    const handleButtonClick = (event: MouseEvent<HTMLElement>) => {
+    const handleCountryButtonClick = (event: MouseEvent<HTMLElement>) => {
         event.stopPropagation();
         event.preventDefault();
-        toggleIsOpen();
+        toggleCountrySelectDropdown();
     };
+
+    const handleStateButtonClick = (event: MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        event.stopPropagation();
+        toggleStateSelectDropdown();
+    }
 
     const handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         setSearchQuery(event.target.value);
@@ -58,31 +78,99 @@ const Checkout = () => {
         event.stopPropagation();
         const updatedCountry = (event.target as HTMLElement).textContent; // divs, spans, buttons i.e. "non-form-controls" don't have a `value` attribute so event.target.value doesn't work. Use textContent instead
         setSelectedCountry(updatedCountry || ""); // use empty string if textContent is null
-        setIsOpen(false);
+        setIsCountriesSelectDropdownOpen(false);
         setSearchQuery("");
+    };
+
+    const handleStateSelect = (event: MouseEvent) => {
+        event.stopPropagation();
+        const updatedState = (event.target as HTMLElement).textContent;
+        setSelectedState(updatedState);
+        setIsStateDropdownOpen(false);
+    };
+
+    const clearFormInputs = () => {
+        setFormInputs({email: "", firstName: "", lastName: "", company: "", streetAddress: "", city: "", state: "", zipOrPostalCode: "", additionalAddress: ""})
+    }
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handlePayment = async (event) => {
+        event.preventDefault();
+
+        if(!stripe || !elements) return;
+
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({amount: 10000})
+        }).then(response => response.json());
+
+        console.log(response);
+
+        const {paymentIntent: {client_secret}} = response;
+        console.log(client_secret);
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: currentUser ? currentUser.displayName : `${firstName} + ${lastName}`,
+                    email: currentUser ? currentUser.email : email,
+                    address: additionalAddress.length ? `${streetAddress} + ${additionalAddress}` : streetAddress
+                },
+            },
+        });
+
+        if(paymentResult.error){
+            alert(paymentResult.error)
+        } else {
+            if(paymentResult.paymentIntent.status === 'succeeded') {
+                alert("Payment Successful!");
+                console.log(paymentResult);
+                clearFormInputs();
+            };
+        };
     };
 
     useEffect(() => {
         const handleOutsideDropdownClick = (event: MouseEvent<HTMLElement>) => {
-            if(countriesDropdownRef.current && !countriesDropdownRef.current.contains(event.target as HTMLElement)){
-                setIsOpen(false);
-            }
+            const clickedElement = event.target as HTMLElement
+            if(countriesDropdownRef.current && !countriesDropdownRef.current.contains(clickedElement)){
+                setIsCountriesSelectDropdownOpen(false);
+            };
         };
 
         document.body.addEventListener("click", handleOutsideDropdownClick);
 
         return () => document.body.removeEventListener("click", handleOutsideDropdownClick);
-    },[isOpen])
+    },[isCountriesSelectDropdownOpen])
 
     useEffect(() => {
         const filteredCountrySearch = countries.filter((country) => country.toLowerCase().includes(searchQuery.toLowerCase()));
         setFilteredCountries(filteredCountrySearch);
     },[searchQuery, countriesArray])
 
+    useEffect(() => {
+        const handleOutsideStateDropdownClick = (event: MouseEvent<HTMLElement>) => {
+            const clickedElement = event.target as HTMLElement;
+            if(stateDropdownRef.current && !stateDropdownRef.current.contains(clickedElement)){
+                setIsStateDropdownOpen(false);
+            };
+        };
+
+        document.body.addEventListener("click", handleOutsideStateDropdownClick);
+
+        return () => document.body.removeEventListener("click", handleOutsideStateDropdownClick);
+    }, [isStateDropdownOpen])
+
     return (
         <CheckoutContainer>
             <FormContainer>
-                <form>
+                <form onSubmit={handlePayment}>
                     <EmailContainer>
                         <label>Contact</label>
                         <CheckoutFormInput label="Email" type="email" name="email" value={email} onChange={handleInputChange} required></CheckoutFormInput>
@@ -91,25 +179,24 @@ const Checkout = () => {
                     <DeliveryContainer>
                         <label id="delivery-label">Delivery</label>
                         
-                        <DropdownContainer>
-                            <label htmlFor="countrySelect" onClick={handleButtonClick}>Select Country/Region</label>
-                            <DropdownButton type="button" onClick={handleButtonClick}>
+                        <CountriesDropdownContainer>
+                            <label htmlFor="countrySelect" onClick={handleCountryButtonClick}>Select Country/Region</label>
+                            <CountriesDropdownButton type="button" onClick={handleCountryButtonClick}>
                                 {selectedCountry}
-                            </DropdownButton>
+                            </CountriesDropdownButton>
 
-                            <DropdownContent open={isOpen} ref={countriesDropdownRef}>
+                            <CountriesDropdownContent isOpen={isCountriesSelectDropdownOpen} ref={countriesDropdownRef}>
                                 <SearchInputContainer>
                                     <SearchInput type='text' placeholder='Search Countries...' value={searchQuery} onChange={handleSearchInputChange}></SearchInput>
                                 </SearchInputContainer>
 
-                                <OptionsContainer>
+                                <CountriesOptionsContainer>
                                     {filteredCountries.map((country) => (
-                                        <DropdownOption key={country} onClick={handleCountrySelect}>{country}</DropdownOption>
+                                        <CountriesDropdownOption key={country} onClick={handleCountrySelect}>{country}</CountriesDropdownOption>
                                     ))}
-                                </OptionsContainer>
-                            </DropdownContent>
-
-                        </DropdownContainer>
+                                </CountriesOptionsContainer>
+                            </CountriesDropdownContent>
+                        </CountriesDropdownContainer>
                         
                         <NameContainer>
                             <CheckoutFormInput label="First Name" type="text" name="firstName" value={firstName} onChange={handleInputChange} required></CheckoutFormInput>
@@ -117,8 +204,25 @@ const Checkout = () => {
                         </NameContainer>
 
                         <AddressContainer>
-                            <CheckoutFormInput label="Company (optional)" type="text" name="company" value={company} onChange={handleInputChange} className="address-input"></CheckoutFormInput>
-                            <CheckoutFormInput label="Address" type="text" name="address" value={address} onChange={handleInputChange} className="address-input" required ></CheckoutFormInput>
+                            <CheckoutFormInput label="Company (optional)" type="text" name="company" value={company} onChange={handleInputChange}></CheckoutFormInput>
+                            <CheckoutFormInput label="Address" type="text" name="streetAddress" value={streetAddress} onChange={handleInputChange} required ></CheckoutFormInput>
+
+                            <CityStateZipcodeContainer>
+                                <CheckoutFormInput label="City" name="city" value={city} required></CheckoutFormInput>
+                                        
+                                <StatesDropdownContainer>
+                                    <label onClick={handleStateButtonClick}>State</label>
+                                    <StatesDropdownButton onClick={handleStateButtonClick}>{selectedState}</StatesDropdownButton>
+
+                                    <StatesDropdownContent isOpen={isStateDropdownOpen} ref={stateDropdownRef}>
+                                        {statesArray.map((state) => (
+                                            <StatesDropdownOption key={state} onClick={handleStateSelect}>{state}</StatesDropdownOption>
+                                        ))}
+                                    </StatesDropdownContent>
+                                </StatesDropdownContainer>
+
+                                <CheckoutFormInput label="Zip / Postal Code" name="zipOrPostalCode" value={zipOrPostalCode} required></CheckoutFormInput>
+                            </CityStateZipcodeContainer>
 
                             <OptionalAddressInputContainer>
                                 <OptionalAddressText onClick={toggleOptionalAddressInput} className='add'>+ Add appartment, suite, etc</OptionalAddressText>
@@ -128,10 +232,11 @@ const Checkout = () => {
                         </AddressContainer>
  
                     </DeliveryContainer>
+
+                    <PaymentForm></PaymentForm>
                 </form>
             </FormContainer>
 
-            <PaymentForm></PaymentForm>
         </CheckoutContainer>
     );
 };
