1e35eda7d639f7b7a4387cfd09424663ad2e6883
Ben <bennnnnnnvu@gmail.com>
2024-02-15 12:25:49 -0800

    remove index.css

--- src/components/payment-form/updated-payment-form-component.tsx
+++ src/components/payment-form/updated-payment-form-component.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState, FormEvent } from "react";
 import { PaymentElement, useStripe, useElements} from "@stripe/react-stripe-js";
 
 import { PaymentFormContainer, FormContainer, PaymentButton } from "./updated-payment-form-styles";
+import { BUTTON_STYLE_CLASSES } from "../Button/button-style-classes";
 
 const UpdatedPaymentForm = () => {
 
@@ -9,7 +10,8 @@ const UpdatedPaymentForm = () => {
     const stripe = useStripe();
     const elements = useElements();
     
-    //create payment intent on the server
+    //create payment intent with cartTotal amount to Netlify server
+    //send this request to Netlify serverless function
     useEffect(() => {
         const response = fetch('/.netlify/functions/create-payment-intent', {
             method: 'post',
@@ -38,14 +40,22 @@ const UpdatedPaymentForm = () => {
         event.preventDefault();
     
         if(!stripe || !elements) return;
+
+        const {error: submitError} = await elements.submit();
+        if(submitError){
+            console.log(submitError);
+            return;
+        }
     
         const paymentResult = await stripe.confirmPayment({
             elements,
             clientSecret,
             confirmParams: {
-                return_url: ""
+                return_url: "https://sandra-sanchez-art.netlify.app",
             }
         });
+
+        console.log(paymentResult);
     
         if(paymentResult.error){
             alert(paymentResult.error)
@@ -59,9 +69,9 @@ const UpdatedPaymentForm = () => {
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={handlePayment}>
                 <PaymentElement></PaymentElement>
-                <button id="submit">Submit</button>
+                <PaymentButton buttonType={BUTTON_STYLE_CLASSES.google} id="submit">Submit</PaymentButton>
             </FormContainer>
 
         </PaymentFormContainer>
