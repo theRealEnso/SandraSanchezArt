5fae161432a79411228980d26e8d6149e5e1dd74
Ben <bennnnnnnvu@gmail.com>
2024-02-24 15:49:55 -0800

    modify vite.config.ts file, try to add environment variables to fix Uncaught ref error / process not defined

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import vitePluginTs from 'vite-plugin-ts';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vitePluginTs(),],
+  define: {
+    'process.env.VITE_REACT_APP_AWS_REGION': process.env.VITE_REACT_APP_AWS_REGION,
+    'process.env.VITE_REACT_APP_AWS_SECRET_ACCESS_KEY_ID': process.env.VITE_REACT_APP_AWS_SECRET_ACCESS_KEY_ID,
+    'process.env.VITE_REACT_APP_AWS_SECRET_ACCESS_KEY': process.env.VITE_REACT_APP_AWS_SECRET_ACCESS_KEY,
+  }
   // build: {
   //   outDir: 'build',
   // }
